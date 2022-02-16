b6c50047cc7af303658a9c70d3dfe51b44ccde63
FGONZALES16 <francisgonzales@galileo.ph>
2022-02-16 20:30:02 +0800

    added income statement

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 
 urlpatterns = [
     path('stock_history_key_ratio_json', views.stock_history_key_ratio_json, name='home'),
+    path('stock_income_statement_json', views.income_statement_json, name='home'),
 ]
